4778fa11bc667bf29a200b594795778cc4cce195
laurenbenichou <laurenbenichou@gmail.com>
2014-03-10 14:18:31 -0700

    added sharing buttons

--- js/main.js
+++ js/main.js
@@ -143,33 +143,33 @@ function initialize() {
 
 
 // Clicking on a post zooms to that location
-$(".locate_post", ".main-content").on("click", function(){
-  loc = $(this).attr("data-id")
-  geocoder = new google.maps.Geocoder();
-  geocoder.geocode( { 'address': loc}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      var marker = new google.maps.Marker({
-        position: results[0].geometry.location,
-        icon: "/images/marker-orange.png"
-      });
-      marker.setMap(map)
-      map.setZoom(16);
-      map.panTo(results[0].geometry.location);
-    } else {
-      alert("Geocode was not successful for the following reason: " + status);
-    }
-  });
-});
+// $(".locate_post", ".main-content").on("click", function(){
+//   loc = $(this).attr("data-id")
+//   geocoder = new google.maps.Geocoder();
+//   geocoder.geocode( { 'address': loc}, function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//       var marker = new google.maps.Marker({
+//         position: results[0].geometry.location,
+//         icon: "/images/marker-orange.png"
+//       });
+//       marker.setMap(map)
+//       map.setZoom(16);
+//       map.panTo(results[0].geometry.location);
+//     } else {
+//       alert("Geocode was not successful for the following reason: " + status);
+//     }
+//   });
+// });
 
-$(".social-media-1").on("click", function(){
-  map.panTo(new google.maps.LatLng(37.76, -122.436));
-  map.setZoom(12);
-});
+// $(".social-media-1").on("click", function(){
+//   map.panTo(new google.maps.LatLng(37.76, -122.436));
+//   map.setZoom(12);
+// });
 
-$(".back-arrow", ".main-content").on("click", function(){
-  map.panTo(new google.maps.LatLng(37.76, -122.436));
-  map.setZoom(12);
-});
+// $(".back-arrow", ".main-content").on("click", function(){
+//   map.panTo(new google.maps.LatLng(37.76, -122.436));
+//   map.setZoom(12);
+// });
 
 };
 
